d8b6694c1fbd0b79648f074295f677ee55217150
chdrchz <saavannaa.rose@gmail.com>
2025-03-13 11:03:16 -0500

    styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,15 @@
-import "@/styles/Header/styles.css";
 import Button from "./Button";
+import { Film } from "lucide-react";
+
+import "@/styles/Header/styles.css";
 
 export default function Header() {
     return(
         <div className="container-header">
-            <p>Cinema Guru</p>
+            <div className="app-title">
+                <Film />
+                <p>Cinema Guru</p>
+            </div>
             <div className="container-header-user">
                 <p>Welcome, user!</p>
                 <Button>Logout</Button>
