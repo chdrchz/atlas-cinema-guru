4ab127eff0fcbcae6cb7146c3f92992c5ab2b75d
chdrchz <saavannaa.rose@gmail.com>
2025-03-13 09:06:14 -0500

    stuff?

--- components/FilterAndSearch.tsx
+++ components/FilterAndSearch.tsx
@@ -1,13 +1,23 @@
 import Input from "./Inputs/Input";
-import InputTwo from "./Inputs/InputTwo";
+
+import "@/styles/FilterAndSearch/styles.css"
 
 export default function FilterAndSearch() {
     return (
         <div className="container-filter">
-            <Input placeholder="Search Movies"/>
+            <div className="search-movies">
+                <p>Search</p>
+                <Input placeholder="Search Movies"/>
+            </div>
             <div className="min-max-year">
-                <InputTwo placeholder="Min Year"/>
-                <InputTwo placeholder="Max Year"/>
+                <div className="min-year">
+                    <h1>Min Year</h1>
+                    <Input placeholder="Min Year"/>
+                </div>
+                <div className="max-year">
+                    <h1>Max Year</h1>
+                    <Input placeholder="Max Year"/>
+                </div>
             </div>
         </div>
     )
--- components/Inputs/InputTwo.tsx
+++
@@ -1,7 +0,0 @@
-import "@/styles/Input/styles.css";
-
-export default function Input({ placeholder }: { placeholder: React.ReactNode }) {
-    return (
-        <input className="input-two" placeholder={String(placeholder)} />
-    );
-}
