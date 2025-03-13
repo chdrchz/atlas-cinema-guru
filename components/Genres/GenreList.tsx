7638e14c6f4e23cb61ab07c4ae25af49e9e84195
chdrchz <saavannaa.rose@gmail.com>
2025-03-13 09:52:21 -0500

    styles

--- components/Genres/GenreList.tsx
+++ components/Genres/GenreList.tsx
@@ -2,15 +2,20 @@ import GenreOption from "./GenreOption"
 
 export default function GenreList() {
     return (
-        <div className="genre-list">
-            <GenreOption title="genre 1" />
-            <GenreOption title="genre 2" />
-            <GenreOption title="genre 3" />
-            <GenreOption title="genre 4" />
-            <GenreOption title="genre 5" />
-            <GenreOption title="genre 6" />
-            <GenreOption title="genre 7" />
-            <GenreOption title="genre 8" />
+        <div className="genre-container">
+            <div className="genre-heading">
+                <p>Genres</p>
+            </div>
+            <div className="genre-list">
+                <GenreOption title="genre 1" />
+                <GenreOption title="genre 2" />
+                <GenreOption title="genre 3" />
+                <GenreOption title="genre 4" />
+                <GenreOption title="genre 5" />
+                <GenreOption title="genre 6" />
+                <GenreOption title="genre 7" />
+                <GenreOption title="genre 8" />
+            </div>
         </div>
     )
 }
