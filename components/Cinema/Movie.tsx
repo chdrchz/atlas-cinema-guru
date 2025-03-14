58fb53ce5f460d6ef77d36bfb5f3df174b7b6054
chdrchz <saavannaa.rose@gmail.com>
2025-03-14 13:24:26 -0500

    movie list is resposinve now

--- components/Cinema/Movie.tsx
+++ components/Cinema/Movie.tsx
@@ -4,7 +4,7 @@ import PlaceholderSVG from "@/assets/placeholder.svg";
 export default function Movie() {
     return (
         <div>
-            <Image src={PlaceholderSVG} alt="Movie Placeholder" width={300} height={300} />
+            <Image src={PlaceholderSVG} alt="Movie Placeholder" width={500} height={500} />
         </div>
     )
 }
