d61a67a658ca032c50e265a95721678c94dad3b2
chdrchz <saavannaa.rose@gmail.com>
2025-03-17 09:49:20 -0500

    sign out button works

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,23 @@
+"use client";
+
 import Button from "./Button";
 import { Film } from "lucide-react";
+
+import { signOut } from "next-auth/react";
 
 import "@/styles/Header/styles.css";
 
 export default function Header() {
-    return(
-        <div className="container-header">
-            <div className="app-title">
-                <Film />
-                <p>Cinema Guru</p>
-            </div>
-            <div className="container-header-user">
-                <p>Welcome, user!</p>
-                <Button className="button-header">Logout</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="container-header">
+      <div className="app-title">
+        <Film />
+        <p>Cinema Guru</p>
+      </div>
+      <div className="container-header-user">
+        <p>Welcome, user!</p>
+        <Button className="button-header" onClick={() => signOut()}>Logout</Button>
+      </div>
+    </div>
+  );
 }
