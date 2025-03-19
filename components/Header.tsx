31d92795f89055beb13d1f9db41f388cf8109ff9
chdrchz <saavannaa.rose@gmail.com>
2025-03-19 14:48:30 -0500

    user name displayed in the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,12 +2,16 @@
 
 import Button from "./Button";
 import { Film } from "lucide-react";
-
+import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 
 import "@/styles/Header/styles.css";
 
 export default function Header() {
+  const { data: session } = useSession();
+
+  const userName = session?.user?.name || "Guest";
+
   return (
     <div className="container-header">
       <div className="app-title">
@@ -15,8 +19,10 @@ export default function Header() {
         <p>Cinema Guru</p>
       </div>
       <div className="container-header-user">
-        <p>Welcome, user!</p>
-        <Button className="button-header" onClick={() => signOut()}>Logout</Button>
+        <p>Welcome, {userName}!</p>
+        <Button className="button-header" onClick={() => signOut()}>
+          Logout
+        </Button>
       </div>
     </div>
   );
